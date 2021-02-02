3907e2749a174532225eccd91048805634933499
yumetodo <yume-wikijp@live.jp>
2021-02-02 20:25:13 +0900

    chore: use fallthrough attribute

--- Siv3D/src/Siv3D/Hash/SivHash.cpp
+++ Siv3D/src/Siv3D/Hash/SivHash.cpp
@@ -47,12 +47,12 @@ namespace s3d
 
 			switch (len & 7)
 			{
-			case 7: h ^= uint64(data2[6]) << 48;
-			case 6: h ^= uint64(data2[5]) << 40;
-			case 5: h ^= uint64(data2[4]) << 32;
-			case 4: h ^= uint64(data2[3]) << 24;
-			case 3: h ^= uint64(data2[2]) << 16;
-			case 2: h ^= uint64(data2[1]) << 8;
+			case 7: h ^= uint64(data2[6]) << 48; [[fallthrough]];
+			case 6: h ^= uint64(data2[5]) << 40; [[fallthrough]];
+			case 5: h ^= uint64(data2[4]) << 32; [[fallthrough]];
+			case 4: h ^= uint64(data2[3]) << 24; [[fallthrough]];
+			case 3: h ^= uint64(data2[2]) << 16; [[fallthrough]];
+			case 2: h ^= uint64(data2[1]) << 8; [[fallthrough]];
 			case 1: h ^= uint64(data2[0]);
 				h *= m;
 			};
