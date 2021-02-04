0b18d7615b498278b9b17fb37ee4f38a2e7f175a
yumetodo <yume-wikijp@live.jp>
2021-02-05 00:11:37 +0900

    fix: SystemStatusFlag was not provided in mingw heaader

--- Siv3D/src/Siv3D-Platform/WindowsDesktop/PowerStatus/SivPowerStatus.cpp
+++ Siv3D/src/Siv3D-Platform/WindowsDesktop/PowerStatus/SivPowerStatus.cpp
@@ -11,11 +11,35 @@
 
 # include <Siv3D/PowerStatus.hpp>
 # include <Siv3D/Windows.hpp>
-
+#ifndef SYSTEM_STATUS_FLAG_POWER_SAVING_ON
+# define SYSTEM_STATUS_FLAG_POWER_SAVING_ON  0x01
+#endif
 namespace s3d
 {
 	namespace System
 	{
+		namespace detail {
+			struct HasSystemStatusFlagImpl
+			{
+				template <class T>
+				static std::true_type check(decltype(T::SystemStatusFlag)*);
+				template <class T>
+				static std::false_type check(...);
+			};
+			template <class T>
+			struct HasSystemStatusFlag : public decltype(HasSystemStatusFlagImpl::check<T>(nullptr)) {};
+			template<typename Target = SYSTEM_POWER_STATUS>
+			DWORD GetSystemStatusFlagImpl(const Target& s) noexcept
+			{
+				if constexpr (HasSystemStatusFlag<SYSTEM_POWER_STATUS>::value) {
+					return s.SystemStatusFlag;
+				}
+				else {
+					return s.Reserved1;
+				}
+			}
+			inline DWORD GetSystemStatusFlag(const SYSTEM_POWER_STATUS& s) noexcept  { return GetSystemStatusFlagImpl(s); }
+		}
 		PowerStatus GetPowerStatus()
 		{
 			SYSTEM_POWER_STATUS s;
@@ -38,7 +62,7 @@ namespace s3d
 				: BatteryStatus::Middle;
 
 			status.charging = !!(s.BatteryFlag & BATTERY_FLAG_CHARGING);
-			status.batterySaver = !!(s.SystemStatusFlag & SYSTEM_STATUS_FLAG_POWER_SAVING_ON);
+			status.batterySaver = !!(s3d::System::detail::GetSystemStatusFlag(s) & SYSTEM_STATUS_FLAG_POWER_SAVING_ON);
 
 			if (s.BatteryLifePercent != BATTERY_PERCENTAGE_UNKNOWN)
 			{
